8857f9e11fec9c526b99c010b8620113c86d4357
akmich <akmich@mail.ru>
2026-09-17 16:38:14 +0300

    все сделал с допом

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    private Integer id;
+
+    @NotBlank(message = "Электронная почта не может быть пустой")
+    @Email(message = "Электронная почта должна соответствовать формату")
+    private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не должен содержать пробелы") // Регулярное выражение: запрет любых пробельных символов
+    private String login;
+
+    private String name;
+
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+}
